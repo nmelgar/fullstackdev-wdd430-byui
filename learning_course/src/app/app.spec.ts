a4cc75cf437b04a398f46752636a41f566027e67
nmelgar <nmelgar16013@gmail.com>
2025-09-24 20:19:47 -0600

    Structure the repo again, to better divide learning and projects.

--- learning_course/src/app/app.spec.ts
+++ learning_course/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, week1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, learning_course');
   });
 });
