a1778681140f98bed8911fc5ed7ee2a55aa23193
nmelgar <nmelgar16013@gmail.com>
2025-09-29 20:39:52 -0600

    Work w/event binding and interpolation.

--- learning_course/src/app/app.ts
+++ learning_course/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServerComponent } from './server/server.component';
-import { Servers } from './servers/servers';
+import { ServersComponent } from './servers/servers';
 import { SuccessAlert } from './success-alert/success-alert';
 import { WarningAlert } from './warning-alert/warning-alert';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ServerComponent, ServerComponent, Servers, SuccessAlert, WarningAlert],
+  imports: [
+    RouterOutlet,
+    ServerComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlert,
+    WarningAlert,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- learning_course/src/app/servers/servers.ts
+++ learning_course/src/app/servers/servers.ts
@@ -4,8 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   imports: [],
   templateUrl: './servers.html',
-  styleUrl: './servers.css'
+  styleUrl: './servers.css',
 })
-export class Servers {
+export class ServersComponent {
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
+  userName: string = '';
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onUpdateUserName(event: any) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
 }
