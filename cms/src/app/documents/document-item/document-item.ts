628ddcb95adf7029d4b39b36fa42c796d455bdf5
nmelgar <nmelgar16013@gmail.com>
2025-10-18 14:53:48 -0600

    Implement the DocumentItemComponent

--- cms/src/app/documents/document-item/document-item.ts
+++ cms/src/app/documents/document-item/document-item.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-item',
   imports: [],
   templateUrl: './document-item.html',
-  styleUrl: './document-item.css'
+  styleUrl: './document-item.css',
 })
 export class DocumentItem {
-
+  @Input() document!: Document;
 }
