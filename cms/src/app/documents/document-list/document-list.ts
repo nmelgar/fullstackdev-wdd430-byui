24faa078e583f6e0d482222359ae8957231b86a1
nmelgar <nmelgar16013@gmail.com>
2025-10-18 21:53:27 -0600

    Implement the DocumentListComponent

--- cms/src/app/documents/document-list/document-list.ts
+++ cms/src/app/documents/document-list/document-list.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { DocumentItem } from '../document-item/document-item';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,4 +8,17 @@ import { DocumentItem } from '../document-item/document-item';
   templateUrl: './document-list.html',
   styleUrl: './document-list.css',
 })
-export class DocumentList {}
+export class DocumentList {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('1', 'Document 1', 'Document 1 Details.', 'Document 1 URL', []),
+    new Document('1', 'Document 2', 'Document 2 Details.', 'Document 2 URL', []),
+    new Document('1', 'Document 3', 'Document 3 Details.', 'Document 3 URL', []),
+    new Document('1', 'Document 4', 'Document 4 Details.', 'Document 4 URL', []),
+    new Document('1', 'Document 5', 'Document 5 Details.', 'Document 5 URL', []),
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
+}
