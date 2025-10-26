2324cc27876a55605b5f3783dd85a011c6fb9d8f
nmelgar <nmelgar16013@gmail.com>
2025-10-25 21:00:19 -0600

    Injecting and Using the Contact Service. Images not displaying correctly.

--- cms/src/app/contacts/contact-list/contact-list.ts
+++ cms/src/app/contacts/contact-list/contact-list.ts
@@ -1,37 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 import { ContactItem } from '../contact-item/contact-item';
+import { ContactService } from '../contact.service'; // ✅ Import the service
 
 @Component({
   selector: 'app-contact-list',
   imports: [ContactItem],
   templateUrl: './contact-list.html',
-  styleUrl: './contact-list.css',
+  styleUrls: ['./contact-list.css'],
 })
-export class ContactList {
-  contacts: Contact[] = [
-    new Contact(
-      '234',
-      'John Doe',
-      'qwer@gmail.com',
-      '801-555-5555',
-      'https://www.w3schools.com/howto/img_avatar.png'
-    ),
+export class ContactList implements OnInit {
+  contacts: Contact[] = [];
 
-    new Contact(
-      '235',
-      'John Does',
-      'qwer@gmail.com',
-      '801-555-5555',
-      'https://www.w3schools.com/howto/img_avatar.png'
-    ),
-  ];
-  
   @Output() selectedContactEvent = new EventEmitter<Contact>();
 
-  onSelectContact(contact: Contact) {
-    console.log('Contact clicked and emitting:', contact.name); 
-    this.selectedContactEvent.emit(contact);
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.contacts = this.contactService.getContacts();
   }
 
+  onSelectContact(contact: Contact) {
+    console.log('Contact clicked and emitting:', contact.name);
+    this.selectedContactEvent.emit(contact);
+  }
 }
