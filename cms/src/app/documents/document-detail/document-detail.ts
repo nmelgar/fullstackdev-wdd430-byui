9939dca8f723e611d4c170e5a83bd3aeb9252974
nmelgar <nmelgar16013@gmail.com>
2025-11-06 22:33:22 -0600

    Define a [routerLink] Directive for the Edit Button

--- cms/src/app/documents/document-detail/document-detail.ts
+++ cms/src/app/documents/document-detail/document-detail.ts
@@ -7,29 +7,24 @@ import { DocumentService } from '../document.service';
 @Component({
   selector: 'app-document-detail',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule, RouterLink],
   templateUrl: './document-detail.html',
   styleUrl: './document-detail.css',
 })
 export class DocumentDetail implements OnInit {
-  document: Document | undefined | null; 
+  document: Document | undefined | null;
 
-  constructor(
-    private documentService: DocumentService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private documentService: DocumentService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (params: Params) => {
-        const id = params['id'];
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
 
-        if (id) {
-          this.document = this.documentService.getDocument(id); 
-        } else {
-          this.document = null; 
-        }
+      if (id) {
+        this.document = this.documentService.getDocument(id);
+      } else {
+        this.document = null;
       }
-    );
+    });
   }
 }
--- cms/src/app/documents/document-edit/document-edit.ts
+++ cms/src/app/documents/document-edit/document-edit.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-document-edit',
+  standalone: true,
   imports: [],
   templateUrl: './document-edit.html',
-  styleUrl: './document-edit.css'
+  styleUrl: './document-edit.css',
 })
-export class DocumentEdit {
-
-}
+export class DocumentEdit {}
